fed90c3fcd91ad7e0741ecd1389c640e69ad3c80
Be Mai <bemai@gmail.com>
2022-03-01 22:41:43 +0700

    add reponsive

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ var swiper2 = new Swiper('.bg-slider', {
     swiper: swiper,
   },
 })
+
+// Navigation bar effects on scroll
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header')
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+//Reponsive menu
+const menuBtn = document.querySelector('.nav-menu-btn')
+const closeBtn = document.querySelector('.nav-close-btn')
+const navigation = document.querySelector('.navigation')
+
+menuBtn.addEventListener('click', () => {
+  navigation.classList.add('active')
+})
+
+closeBtn.addEventListener('click', () => {
+  closeBtn.classList.remove('active')
+})
